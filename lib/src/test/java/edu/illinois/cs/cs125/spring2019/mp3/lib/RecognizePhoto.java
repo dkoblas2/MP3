6b65b3c8de1c50d8ca77d433eab794f7a23348ed
duncankoblas <dkoblas2@illinois.edu>
2019-03-05 09:39:18 -0600

    mp3

---
+++ lib/src/test/java/edu/illinois/cs/cs125/spring2019/mp3/lib/RecognizePhoto.java
@@ -0,0 +1,38 @@
+public class RecognizePhoto {
+
+
+    public RecognizePhoto () {
+
+    }
+
+
+    public static java.lang.String getCaption(java.lang.String json) {
+
+    }
+
+    public static java.lang.String getFormat(java.lang.String json) {
+
+    }
+
+    public static int getHeight(java.lang.String json) {
+
+    }
+
+    public static int getWidth(java.lang.String json) {
+
+    }
+
+    public static boolean isACat(java.lang.String json, double minConfidence) {
+
+    }
+
+    public static boolean isADog(java.lang.String json, double minConfidence) {
+
+    }
+
+    public static boolean isRick(java.lang.String json) {
+
+    }
+
+
+}
