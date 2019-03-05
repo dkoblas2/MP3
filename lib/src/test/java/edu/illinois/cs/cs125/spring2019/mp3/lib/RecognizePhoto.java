481febd06b60dc0ff5003010bc242c7f51c32ffd
duncankoblas <dkoblas2@illinois.edu>
2019-03-05 10:39:07 -0600

    mp3

--- lib/src/test/java/edu/illinois/cs/cs125/spring2019/mp3/lib/RecognizePhoto.java
+++ lib/src/test/java/edu/illinois/cs/cs125/spring2019/mp3/lib/RecognizePhoto.java
@@ -22,6 +22,9 @@ public final class RecognizePhoto {
     }
 
     public static int getHeight(java.lang.String json) {
+        if (json == null) {
+            return 0;
+        }
         JsonParser parser = new JsonParser();
         JsonObject result = parser.parse(json).getAsJsonObject();
         return result.get("height").getAsInt();
