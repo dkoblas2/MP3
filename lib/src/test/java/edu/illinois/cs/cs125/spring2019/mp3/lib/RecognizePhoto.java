a6dd9ecbb54b8bb99e2307c9d53ab393b8396e5f
duncankoblas <dkoblas2@illinois.edu>
2019-03-05 10:35:57 -0600

    mp3

--- lib/src/test/java/edu/illinois/cs/cs125/spring2019/mp3/lib/RecognizePhoto.java
+++ lib/src/test/java/edu/illinois/cs/cs125/spring2019/mp3/lib/RecognizePhoto.java
@@ -1,5 +1,12 @@
 package edu.illinois.cs.cs125.spring2019.mp3.lib;
 
+import
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import jdk.nashorn.internal.parser.JSONParser;
+
 public final class RecognizePhoto {
 
 
@@ -17,7 +24,9 @@ public final class RecognizePhoto {
     }
 
     public static int getHeight(java.lang.String json) {
-        return 0;
+        JsonParser parser = new JsonParser();
+        JsonObject result = parser.parse(json).getAsJsonObject();
+        return result.get("height").getAsInt();
     }
 
     public static int getWidth(java.lang.String json) {
