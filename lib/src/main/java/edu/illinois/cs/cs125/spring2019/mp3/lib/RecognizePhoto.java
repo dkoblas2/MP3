784d29e8512656d1961356c4c58b6980b92aabdb
duncankoblas <dkoblas2@illinois.edu>
2019-03-08 13:52:15 -0600

    mp3

--- lib/src/main/java/edu/illinois/cs/cs125/spring2019/mp3/lib/RecognizePhoto.java
+++ lib/src/main/java/edu/illinois/cs/cs125/spring2019/mp3/lib/RecognizePhoto.java
@@ -56,7 +56,7 @@ public final class RecognizePhoto {
         JsonParser parser = new JsonParser();
         JsonObject result = parser.parse(json).getAsJsonObject();
         for (int i = 0; i < result.get("description").getAsJsonObject().get("tags").getAsJsonArray().size(); i++) {
-            if (result.get("description").getAsJsonObject().get("tags").getAsString().contains("cat")) {
+            if (result.get("description").getAsJsonObject().get("tags").getAsJsonObject().get("cat") != null) {
                 return true;
             }
         }
