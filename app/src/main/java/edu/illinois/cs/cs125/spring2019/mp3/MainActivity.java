e44146fa1e6fa091a5bd85f83ddacad8226f2600
duncankoblas <dkoblas2@illinois.edu>
2019-03-12 10:22:55 -0500

    mp3

--- app/src/main/java/edu/illinois/cs/cs125/spring2019/mp3/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/spring2019/mp3/MainActivity.java
@@ -32,12 +32,10 @@ import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
-import org.json.JSONObject;
 
 import java.io.File;
 import java.io.IOException;
